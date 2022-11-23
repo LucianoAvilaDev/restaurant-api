c7f96a6ccce17d7f052fb51c9766b9e83c176283
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-11-23 15:06:57 -0300

    migrations

---
+++ database/migrations/1669225709993_order_items.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'order_items'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.text('observation','mediumtext')
+      table.decimal('quantity', 6, 2).notNullable()
+      table.decimal('price', 8, 2).notNullable()
+      table.integer('meal_id').unsigned().references('meals.id')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1669225978571_orders.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'orders'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.dateTime('date', { useTz: true }).defaultTo(this.now())
+      table.integer('client_id').unsigned().references('clients.id')
+      table.integer('table_id').unsigned().references('tables.id')
+      table.decimal('total_value', 8, 2).notNullable()
+      table.decimal('paid_value', 8, 2).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
+

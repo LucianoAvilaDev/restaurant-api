b484a2ff7007db13ec56898ca359bec3e5ad464b
Breno Santos Almeida <breno098526049@gmail.com>
2022-12-09 10:11:36 -0300

    perf(promisse return): returning promisse direcly

--- app/Services/UsersServices/GetCurrentUserFullDataService.ts
+++ app/Services/UsersServices/GetCurrentUserFullDataService.ts
@@ -1,21 +1,17 @@
-import { AuthContract } from "@ioc:Adonis/Addons/Auth"
-import User from "App/Models/User"
-import { ServiceReturnType } from "App/Types/types"
+import { AuthContract } from '@ioc:Adonis/Addons/Auth'
+import User from 'App/Models/User'
 
 export default class GetCurrentUserFullDataService {
-
   public static async run(auth: AuthContract): Promise<User> {
-
     const userId: number | undefined = auth.use('api').user?.id
 
-    if (!userId)
-      throw Error("Usuário não conectado")
+    if (!userId) throw Error('Usuário não conectado')
 
-    const user: User = await User.query().preload('role', (roleQuery) => {
-      roleQuery.preload('permissions')
-    }).where('id', userId).firstOrFail()
-
-    return user
-
+    return User.query()
+      .preload('role', (roleQuery) => {
+        roleQuery.preload('permissions')
+      })
+      .where('id', userId)
+      .firstOrFail()
   }
 }

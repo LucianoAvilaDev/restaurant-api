5fe36e1f146eac0da00921026c95e5f5b3d0f632
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-12-13 08:14:23 -0300

    fix(merge): merge changes
    alterações de merge para alinhamento

--- app/Services/UsersServices/GetCurrentUserFullDataService.ts
+++ app/Services/UsersServices/GetCurrentUserFullDataService.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { AuthContract } from "@ioc:Adonis/Addons/Auth"
 import User from "App/Models/User"
-=======
-import { AuthContract } from '@ioc:Adonis/Addons/Auth'
-import User from 'App/Models/User'
->>>>>>> 171371d2b854419c14b1b97483bbf4f77c6029ca
 
 export default class GetCurrentUserFullDataService {
   public static async run(auth: AuthContract): Promise<User> {

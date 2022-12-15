9f6f8e46d4334e33069a072196ac787cf7f55b44
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-12-15 15:45:14 -0300

    fix(api): api
    api

--- app/Controllers/Http/filters/MealsFiltersController.ts
+++ app/Controllers/Http/filters/MealsFiltersController.ts
@@ -2,20 +2,18 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Meal from 'App/Models/Meal'
 
 export default class MealsFiltersController {
-    public async handle({ response, request }: HttpContextContract) {
+  public async handle({ response, request }: HttpContextContract) {
 
-        const { name, type } = request.body()
+    const { name, type } = request.body()
 
-        const meals: Meal[] = await Meal.query().preload('mealType')
-            .if(name, (mealQuery) => {
-                mealQuery.where('name', "like", `%${name}%`)
-            })
-            .if(type, (mealQuery) => {
-                mealQuery.whereHas('mealType', (mealTypeQuery) => {
-                    mealTypeQuery.where('name', 'like', `%${type}%`)
-                })
-            })
+    const meals: Meal[] = await Meal.query().preload('mealType')
+      .if(name, (mealQuery) => {
+        mealQuery.where('name', "like", `%${name}%`)
+      })
+      .if(type, (mealQuery) => {
+        mealQuery.where('mealTypeId', type)
+      })
 
-        return response.ok(meals)
-    }
+    return response.ok(meals)
+  }
 }
--- app/Controllers/Http/filters/TablesFiltersController.ts
+++ app/Controllers/Http/filters/TablesFiltersController.ts
@@ -2,18 +2,23 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Table from 'App/Models/Table'
 
 export default class TablesFiltersController {
-    public async handle({ response, request }: HttpContextContract) {
+  public async handle({ response, request }: HttpContextContract) {
 
-        const { number, isAvailable } = request.body()
+    const { number, isAvailable } = request.body()
 
-        const Tables: Table[] = await Table.query()
-            .if(number, (TableQuery) => {
-                TableQuery.where('number', "like", `%${number}%`)
-            })
-            .if(isAvailable, (TableQuery) => {
-                TableQuery.where('cpf', isAvailable)
-            })
 
-        return response.ok(Tables)
-    }
+
+    const Tables: Table[] = await Table.query()
+      .if(number, (TableQuery) => {
+        TableQuery.where('number', "like", `%${number}%`)
+      })
+      .if(isAvailable && isAvailable == 'true', (TableQuery) => {
+        TableQuery.where('isAvailable', true)
+      })
+      .if(isAvailable && isAvailable == 'false', (TableQuery) => {
+        TableQuery.where('isAvailable', false)
+      })
+
+    return response.ok(Tables)
+  }
 }

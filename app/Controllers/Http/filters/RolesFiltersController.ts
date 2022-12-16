dc48a086734e038fd75e45d09c3ba2360ba6b1c1
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-12-16 16:12:54 -0300

    fix(api): roles
    roles permissions relationship

--- app/Controllers/Http/filters/RolesFiltersController.ts
+++ app/Controllers/Http/filters/RolesFiltersController.ts
@@ -2,15 +2,20 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Role from 'App/Models/Role'
 
 export default class RolesFiltersController {
-    public async handle({ response, request }: HttpContextContract) {
+  public async handle({ response, request }: HttpContextContract) {
 
-        const { name } = request.body()
+    const { name, permission } = request.body()
 
-        const Roles: Role[] = await Role.query()
-            .if(name, (RoleQuery) => {
-                RoleQuery.where('name', "like", `%${name}%`)
-            })
+    const Roles: Role[] = await Role.query().preload('permissions')
+      .if(name, (RoleQuery) => {
+        RoleQuery.where('name', "like", `%${name}%`)
+      })
+      .if(permission, (RoleQuery) => {
+        RoleQuery.whereHas('permissions', (permissionQuery: any) => {
+          permissionQuery.where('id', permission)
+        })
+      })
 
-        return response.ok(Roles)
-    }
+    return response.ok(Roles)
+  }
 }

3330774d88818215e1c5b5db758641d22194a8bd
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-12-16 17:21:46 -0300

    relationship on users flter controller

--- app/Controllers/Http/filters/UsersFiltersController.ts
+++ app/Controllers/Http/filters/UsersFiltersController.ts
@@ -2,19 +2,19 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersFiltersController {
-    public async handle({ response, request }: HttpContextContract) {
+  public async handle({ response, request }: HttpContextContract) {
 
-        const { name, email } = request.body()
+    const { name, email } = request.body()
 
-        const Users: User[] = await User.query()
-            .if(name, (UserQuery) => {
-                UserQuery.where('name', "like", `%${name}%`)
-            })
-            .if(email, (UserQuery) => {
-                UserQuery.where('email', "like", `%${email}%`)
-            })
+    const Users: User[] = await User.query().preload('role')
+      .if(name, (UserQuery) => {
+        UserQuery.where('name', "like", `%${name}%`)
+      })
+      .if(email, (UserQuery) => {
+        UserQuery.where('email', "like", `%${email}%`)
+      })
 
 
-        return response.ok(Users)
-    }
+    return response.ok(Users)
+  }
 }

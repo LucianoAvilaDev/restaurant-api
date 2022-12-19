e27c551a9f37fbad27340df1211cb40b3b46b3ea
Luciano Diniz <luciano.diniz@sagatechbrasil.com.br>
2022-12-19 10:04:51 -0300

    fix(filter): order filters
    correção nos filtros de pedido

--- app/Controllers/Http/filters/OrdersFiltersController.ts
+++ app/Controllers/Http/filters/OrdersFiltersController.ts
@@ -2,28 +2,27 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Order from 'App/Models/Order'
 
 export default class OrdersFiltersController {
-    public async handle({ response, request }: HttpContextContract) {
+  public async handle({ response, request }: HttpContextContract) {
 
-        const { id, client, table, isClosed } = request.body()
+    const { id, client, table, isClosed } = request.body()
 
-        const Orders: Order[] = await Order.query().preload('client').preload('table')
-            .if(id, (OrderQuery: any) => {
-                OrderQuery.where('id', "like", `%${id}%`)
-            })
-            .if(client, (OrderQuery: any) => {
-                OrderQuery.whereHas('client', (OrderTypeQuery: any) => {
-                    OrderTypeQuery.where('name', 'like', `%${client}%`)
-                })
-            })
-            .if(table, (OrderQuery: any) => {
-                OrderQuery.whereHas('table', (OrderTypeQuery: any) => {
-                    OrderTypeQuery.where('number', 'like', `%${table}%`)
-                })
-            })
-            .if(isClosed, (OrderQuery: any) => {
-                OrderQuery.where('isClosed', "like", `%${isClosed}%`)
-            })
+    const Orders: Order[] = await Order.query().preload('client').preload('table')
+      .if(id, (OrderQuery: any) => {
+        OrderQuery.where('id', "like", `%${id}%`)
+      })
+      .if(client, (OrderQuery: any) => {
+        OrderQuery.where('clientId', 'like', `%${client}%`)
+      })
+      .if(table, (OrderQuery: any) => {
+        OrderQuery.where('tableId', 'like', `%${table}%`)
+      })
+      .if(isClosed && isClosed == 'true', (TableQuery) => {
+        TableQuery.where('isClosed', true)
+      })
+      .if(isClosed && isClosed == 'false', (TableQuery) => {
+        TableQuery.where('isClosed', false)
+      })
 
-        return response.ok(Orders)
-    }
+    return response.ok(Orders)
+  }
 }

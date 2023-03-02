7db39c9e462b9823286dbcb9c71f7718db296b64
LucianoAvilaDev <luciano.avila.dev@outlook.com>
2023-03-02 18:09:43 -0300

    logics fixes

--- database/seeders/SystemSeeders/UserSeeder.ts
+++ database/seeders/SystemSeeders/UserSeeder.ts
@@ -1,21 +1,23 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from 'App/Models/User'
+import Hash from '@ioc:Adonis/Core/Hash'
+
 
 export default class extends BaseSeeder {
   public async run() {
     await User.createMany([
       {
         id: 1,
-        name: 'SGS Admin',
+        name: 'Admin',
         email: 'admin@email.com',
-        password: '123456',
+        password: await Hash.make('123456'),
         roleId: 1,
       },
       {
         id: 2,
-        name: 'SGS Collaborator',
+        name: 'Collaborator',
         email: 'collab@email.com',
-        password: '123456',
+        password: await Hash.make('123456'),
         roleId: 2,
       },
     ])

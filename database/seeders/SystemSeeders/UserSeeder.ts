6e34375b2651028b52387e91b97790e87456d664
LucianoAvilaDev <luciano.avila.dev@outlook.com>
2023-03-02 18:02:51 -0300

    seed update

--- database/seeders/SystemSeeders/UserSeeder.ts
+++ database/seeders/SystemSeeders/UserSeeder.ts
@@ -3,23 +3,21 @@ import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run() {
-
     await User.createMany([
       {
         id: 1,
-        name: "SGS Admin",
-        email: "admin.sgs@sagatech.com.br",
-        password: "123456",
-        roleId: 1
+        name: 'SGS Admin',
+        email: 'admin@email.com',
+        password: '123456',
+        roleId: 1,
       },
       {
         id: 2,
-        name: "SGS Collaborator",
-        email: "collab.sgs@sagatech.com.br",
-        password: "123456",
-        roleId: 2
-      }
-
+        name: 'SGS Collaborator',
+        email: 'collab@email.com',
+        password: '123456',
+        roleId: 2,
+      },
     ])
   }
 }
